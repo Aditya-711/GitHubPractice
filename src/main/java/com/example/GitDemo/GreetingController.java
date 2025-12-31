7a74729cc24a80e9900cd07c9b84d4486f9574ab
Aditya Inamdar <adityainamdar711@gmail.com>
2026-01-01 01:24:51 +0530

    Add GreetingController with /greet endpoint

---
+++ src/main/java/com/example/GitDemo/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.GitDemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet() {
+        return new ResponseEntity<String>("hi", HttpStatus.OK);
+    }
+}
