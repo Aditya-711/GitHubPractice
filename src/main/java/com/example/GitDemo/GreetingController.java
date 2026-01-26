a5f989a5a3575e71fd698f52237a72ffdb2ffc5e
Aditya Inamdar <adityainamdar711@gmail.com>
2026-01-26 14:51:45 +0530

    Initial commit

---
+++ src/main/java/com/example/GitDemo/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.GitDemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet() {
+        return new ResponseEntity<String>("hi from DEV_B", HttpStatus.OK);
+    }
+
+
+}
